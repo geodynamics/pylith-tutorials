731ed7264e48e45048a14b192e3d45b6de38281e
Brad Aagaard <baagaard@usgs.gov>
2021-08-26 08:57:00 -0600

    Add presentations 2009-2011.

---
+++ 2011-earthscope/troubleshooting/auto/troubleshooting.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook "troubleshooting"
+ (lambda ()
+    (TeX-add-symbols
+     '("important" 1)
+     '("thispdfpagelabel" 1))
+    (TeX-run-style-hooks
+     "mpmulti"
+     "array"
+     "amsmath"
+     "latex2e"
+     "beamer10"
+     "beamer")))
+
